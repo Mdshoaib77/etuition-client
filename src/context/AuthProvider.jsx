da35ea70d532b1471623e9c5628178271f557f41
Md Shoaib <email4966@gmail.com>
2026-03-14 18:05:58 +0600

    Update AuthProvider.jsx

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,74 @@
+// import { createContext, useEffect, useState } from "react";
+// import { auth, googleProvider } from "../firebase/firebase.init";
+// import {
+//   onAuthStateChanged,
+//   signInWithEmailAndPassword,
+//   createUserWithEmailAndPassword,
+//   signInWithPopup,
+//   signOut,
+// } from "firebase/auth";
+
+// export const AuthContext = createContext(null);
+// const backendUrl = import.meta.env.VITE_API_URL;
+
+// const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   const registerUser = (email, password) =>
+//     createUserWithEmailAndPassword(auth, email, password);
+
+//   const loginUser = (email, password) =>
+//     signInWithEmailAndPassword(auth, email, password);
+
+//   const googleLogin = () => signInWithPopup(auth, googleProvider);
+
+//   const logoutUser = async () => {
+//     setUser(null);
+//     await signOut(auth);
+//   };
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+//       if (!currentUser) {
+//         setUser(null);
+//         setLoading(false);
+//         return;
+//       }
+
+//       // 🔥 get role from backend (JWT cookie auto sent)
+//       const res = await fetch(`${backendUrl}/users/role/${currentUser.email}`, {
+//         credentials: "include",
+//       });
+//       const data = await res.json();
+
+//       setUser({
+//         email: currentUser.email,
+//         role: data.role,
+//       });
+
+//       setLoading(false);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   return (
+//     <AuthContext.Provider
+//       value={{ user, loading, registerUser, loginUser, googleLogin, logoutUser }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default AuthProvider;
+
+
+
+
 import { createContext, useEffect, useState } from "react";
-import { auth, googleProvider } from "../firebase/firebase.init";
+import { auth, googleProvider } from "../Firebase/firebase.init";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
@@ -9,6 +78,8 @@ import {
 } from "firebase/auth";
 
 export const AuthContext = createContext(null);
+
+// API URL from env
 const backendUrl = import.meta.env.VITE_API_URL;
 
 const AuthProvider = ({ children }) => {
